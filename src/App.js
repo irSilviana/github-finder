289d8313e09ca2057fa9231cb4c8971ae385300e
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-03-31 18:25:12 +0700

    Add axios to App component
    Added Life cycle method using componentDidMount(){}
    
    is the method that we can use to make a request (HTTP request) right when the app/component load. Inside this method we can use axios call or fetch API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    let apiUrl = "https://api.github.com/users";
+
+    axios.get(apiUrl).then((res) => console.log(res.data));
+  }
+
   render() {
     return (
       <div className="App">
