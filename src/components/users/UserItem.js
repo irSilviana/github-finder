4bc80181ee41606c2dc9174bdf39b1f3726dd229
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-03-31 18:23:48 +0700

    Add UserItem prototype

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
@@ -22,6 +23,10 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       </div>
     </div>
   );
+};
+
+UserItem.prototype = {
+  user: PropTypes.object.isRequired,
 };
 
 export default UserItem;
