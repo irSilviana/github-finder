1c686d21cc8d86b49206141a9377346542a46456
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-04-01 05:47:29 +0700

    Update User.js
    import Spinner component and add Users.PropTypes

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  render() {
-    const users = this.props.users;
-
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
         {users.map((user) => (
@@ -13,7 +15,13 @@ class Users extends Component {
       </div>
     );
   }
-}
+};
+
+Users.PropTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+
 const userStyle = {
   display: " grid",
   gridTemplateColumns: "repeat(3,1fr) ",
