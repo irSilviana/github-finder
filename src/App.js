a5fe7a0148115fde8993c86031ba3b6950f4169c
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-04-23 08:22:25 +0700

    Add Repos Component & Render RepoItem
    This commit also the end of using the class component

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null,
   };
@@ -59,6 +60,20 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // Get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    let apiUrl = `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`;
+    let clientID = process.env.REACT_APP_GITHUB_CLIENT_ID;
+    let clientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+    const res = await axios.get(
+      `${apiUrl}&client_id=${clientID}&client_secret=${clientSecret}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -69,7 +84,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading, alert } = this.state;
+    const { users, user, repos, loading, alert } = this.state;
     return (
       <Router>
         <div className="App">
@@ -100,7 +115,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
